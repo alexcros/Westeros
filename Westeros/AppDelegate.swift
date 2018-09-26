f4600d620e3fd8683cdc0dbf5f8e8b8ad81e873e
Alex Cros <alexcroscuadros@gmail.com>
2018-09-26 21:10:00 +0200

    add new ui with tabbar controller

--- Westeros/AppDelegate.swift
+++ Westeros/AppDelegate.swift
@@ -16,34 +16,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .cyan
         
         // Model
         let houses = Repository.local.houses
         let seasons = Repository.local.seasons
-
+        let episodes = Repository.local.episodes
+        
+        // OLD INTERFACE
         // Controllers
         // Master
         let houseListVC = HouseListTableViewController(model: houses)
-        let lastHouseSelected = houseListVC.lastSelectedHouse() // reset
-       
+        //        let lastHouseSelected = houseListVC.lastSelectedHouse() // reset
+        let seasonListViewController = SeasonListViewController(model: seasons).wrappedInNavigation()
+        //
+        //        let episodeListViewController = EpisodeListViewController(model: episodes).wrappedInNavigation()
+        //
         // Detail
-        let houseDetailVC = HouseDetailViewController(model: lastHouseSelected)
+        //let houseDetailVC = HouseDetailViewController(model: lastHouseSelected)
+        //
+        //        // Assign delegate
+        //        houseListVC.delegate = houseDetailVC
+        //
+        //        // Combinator for iPad
+        //        let splitVC = UISplitViewController()
+        //        splitVC.viewControllers = [houseListVC.wrappedInNavigation(), houseDetailVC.wrappedInNavigation() ]
         
-        // Assign delegate
-        houseListVC.delegate = houseDetailVC
+        // Extra
+        //let houseCollectionVC = HouseCollectionViewController(model: houses)
         
-        // Combinator for iPad
+        // NUEVA INTERFAZ GRAFICA
+        /*
+         Crea un HouseListViewController empaquetado dentro de un UINavigationController.
+         Crea un SeasonListViewController empaquetado dentro de un UINavigationViewController Mete a ambos dentro de un UITabBarController
+         Usa éste como masterViewController del UISplitViewController. Asegúrate de poder cambiar de uno a otro y poder navegar de House a Person y de Season a Episode.
+         */
+        
+        let newHouseListViewController = HouseListTableViewController(model: houses).wrappedInNavigation()
+        let newSeasonListViewController = SeasonListViewController(model: seasons).wrappedInNavigation()
+        let tabBarController = UITabBarController()
+        
+        let seasonDetailViewController = SeasonDetailViewController(model: seasons.first!)
+        let houseDetailViewController = HouseDetailViewController(model: houses.first!)
+        
+        //let lastHouseSelected = newHouseListViewController.lastSelectedHouse() // reset
+        
+        tabBarController.viewControllers = [newHouseListViewController, newSeasonListViewController]
+        //tabBarController.delegate = self
+        //
+        //        // Combinator for iPad
         let splitVC = UISplitViewController()
-        splitVC.viewControllers = [houseListVC.wrappedInNavigation(), houseDetailVC.wrappedInNavigation()]
+        splitVC.viewControllers = [tabBarController, houseDetailViewController.wrappedInNavigation(), seasonDetailViewController.wrappedInNavigation() ] as! [UIViewController]
         
-        let houseCollectionVC = HouseCollectionViewController(model: houses)
-        
-        window?.rootViewController = houseCollectionVC
+        window?.rootViewController = splitVC
         window?.makeKeyAndVisible()
         
         return true
     }
-
+    
 }
-
