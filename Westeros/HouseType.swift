6030e61fcfc429497555f34ccf267e0c46dc5f9c
Alex Cros <alexcroscuadros@gmail.com>
2018-09-26 20:03:21 +0200

    Created type-safe House enum class

---
+++ Westeros/HouseType.swift
@@ -0,0 +1,15 @@
+//
+//  HouseType.swift
+//  Westeros
+//
+//  Created by Alexandre on 26/9/18.
+//  Copyright © 2018 Alexandre. All rights reserved.
+//
+
+import Foundation
+
+enum HouseType: String {
+    case stark = "Stark"
+    case lannister = "Lannister"
+    case targaryan = "targaryan"
+}
