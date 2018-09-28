0d15e4cb5532d4afc6a8d7551c2cd50d87f2daa9
Alex Cros <alexcroscuadros@gmail.com>
2018-09-28 18:17:27 +0200

    add observe notifcation in VC. Improved UI

--- Westeros/Controllers/EpisodeDetailViewController.swift
+++ Westeros/Controllers/EpisodeDetailViewController.swift
@@ -11,17 +11,20 @@ import UIKit
 class EpisodeDetailViewController: UIViewController {
 
     // MARK: - Outlets
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var broadcastLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
     
     // MARK: - Properties
-    let model: [Episode]!
+    var model: Episode
     
     // MARK: - Initialization
-    init(model: [Episode]) {
+    init(model: Episode) {
         self.model = model
         
         super.init(nibName: nil, bundle: nil)
         
-        title = "Episodes"
+        title = "Episode Info"
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -32,7 +35,36 @@ class EpisodeDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        syncModelWithView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Observe season notification changes
+        NotificationCenter.default.addObserver(self, selector: #selector(seasonDidChange), name: .seasonDidChangeNotification, object: nil)
+    }
+    
+    // MARK: Notifications
+    @objc func seasonDidChange(notification: Notification) {
+        
+        // get season info
+        let info = notification.userInfo!
+        
+        let season = info[Constants.SeasonKey] as? Season
+        
+        let episodes = season?.sortedEpisodes
+        
+        model = (episodes?.first)!
+        syncModelWithView()
+        
+    }
+    
+    // MARK: - Syncro
+    func syncModelWithView() {
+        
+        nameLabel.text = model.fullName
+        broadcastLabel.text = "\(model.broadcastDate)"
+        detailLabel.text = model.description
+        
     }
 
 }
